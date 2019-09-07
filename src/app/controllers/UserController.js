f7e86bdc6d99fe0d2d55ef6685d8a100571c343b
Mauricio Pradella <pradella@gmail.com>
2019-09-07 18:29:37 -0300

    user crud completed

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,24 +3,79 @@ import User from '../models/User';
 import error from '../../utils';
 
 class UserController {
+    constructor() {
+        this.update = this.update.bind(this);
+        this.store = this.store.bind(this);
+    }
+
+    // INDEX
+    async index(req, res) {
+        const allUsers = await User.findAll();
+        return res.json(allUsers);
+    }
+
+    // GET
+    async get(req, res) {
+        const oneUser = await User.findByPk(req.params.id);
+        if (!oneUser) return error(res, 400, 'User does not exist');
+        return res.json(oneUser);
+    }
+
+    // POST
     async store(req, res) {
+        // check input data
+        if (!await this.isValid(req)) {
+            return error(res, 400, 'Validation failed');
+        }
+
+        // check user with this email exists
+        const userExists = await User.findOne({ where: { email: req.body.email } });
+        if (userExists) {
+            return error(res, 400, 'User already exists');
+        }
+
+        // insert
+        const userCreated = await User.create(req.body);
+        return res.json(userCreated);
+    }
+
+    // PUT
+    async update(req, res) {
+        // check input data
+        if (!await this.isValid(req)) {
+            return error(res, 400, 'Validation failed');
+        }
+
+        // check if user exists
+        const targetUser = await User.findByPk(req.params.id);
+        if (!targetUser) return error(res, 400, 'User does not exists');
+
+        // check if is trying to change email to an existing email
+        if (req.body.email !== targetUser.email) {
+            const emailExists = await User.findOne({ where: { email: req.body.email } });
+            if (emailExists) return error(res, 400, 'Email already exists');
+        }
+
+        // update
+        const userUpdated = await targetUser.update(req.body);
+        return res.json(userUpdated);
+    }
+
+    // DELETE
+    async delete(req, res) {
+        const { id } = req.params;
+        const deleteCount = await User.destroy({ where: { id } });
+        if (deleteCount === 0) return error(res, 400, 'Cannot delete user: seems this user does not exists');
+        return res.json({ success: true });
+    }
+
+    async isValid(req) {
         const schema = Yup.object().shape({
             name: Yup.string().required(),
             email: Yup.string().required(),
             password_hash: Yup.string().required().min(4),
         });
-
-        if (!await schema.isValid(req.body)) {
-            return error(res, 400, 'Validation failed');
-        }
-
-        const userExists = User.findOne({ where: { email: req.body.email } });
-        if (userExists) {
-            return error(res, 400, 'User already exists');
-        }
-
-        const userCreated = await User.create(req.body);
-        return res.json(userCreated);
+        return schema.isValid(req.body);
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ import UserController from './app/controllers/UserController';
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.get);
 routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 export default routes;
